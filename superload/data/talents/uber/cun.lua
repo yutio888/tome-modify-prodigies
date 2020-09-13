db3d5e22eba18522e5604cfbf99415095355c07a
yutio888 <yutio888@mail.ustc.edu.cn>
2020-09-13 08:59:54 +0800

    adjust tricks of the trade, slightly increase total dam. merge Massive Blow and You shall be my weapon

--- superload/data/talents/uber/cun.lua
+++ superload/data/talents/uber/cun.lua
@@ -1,4 +1,5 @@
 local Talents = require "engine.interface.ActorTalents"
+local damDesc = Talents.main_env.damDesc
 
 local _M = loadPrevious(...)
 local tricks = Talents.talents_def['T_TRICKS_OF_THE_TRADE']
@@ -7,25 +8,29 @@ if tricks then
     tricks.callbackOnDealDamage = function(self, t, value, target, dead, death_node)
     		if dead or self:attr("dont_tricks") or self:attr("dont_raze") then return end
     		if not self:canSee(target) or not self:hasLOS(target.x, target.y) then return end
+    		if target.__is_actor and target.T_TRICKS_OF_THE_TRADE and target:knowTalent(T_TRICKS_OF_THE_TRADE) then return end
     		local mh, oh = self:hasDualWeapon()
-                if (mh and oh) and oh.subtype ~= "mindstar" then
-                if target.turn_procs.tricks and target.turn_procs.tricks >= 15 then return end
+                if (mh and oh) and oh.subtype == "dagger" then
+                local times = target.turn_procs.tricks or 0
+                local percent = 1 - 0.05 * times
+                if percent <= 0 then return end
     		    self:attr("dont_tricks",1)
-    		    DamageType:get(DamageType.PHYSICAL).projector(self, target.x, target.y, DamageType.PHYSICAL, t.getDamage(self, t))
+    		    DamageType:get(DamageType.PHYSICAL).projector(self, target.x, target.y, DamageType.PHYSICAL, t.getDamage(self, t) * percent)
     		    self:attr("dont_tricks",-1)
-    		    target.turn_procs.tricks = target.turn_procs.tricks or 0
-                target.turn_procs.tricks = target.turn_procs.tricks + 1
+    		    target.turn_procs.tricks = times + 1
     		end
     	end
     tricks.getDamage = function(self, t)
-        return self:combatScale(self:getCun() * self.level, 25, 1250, 75, 6000)
+        return self:combatScale(self:getCun() * self.level, 40, 1250, 90, 5000)
+        --total dam:lv 25 * cun50 * 50%phys inc -> 600;  lv 50 * cun100 * 100%phys inc-> 1800
     end
     tricks.info = function(self, t)
         return ([[You have friends in low places and have learned some underhanded tricks.
         While holding dagger in offhand, whenever you inflict damage to a target in your sight, you deal an additional %.2f physical damage.
-        You cannot trigger more than 10 times on the same target each turn.
+        The target will gain resistance to this effect if it triggers more than one times in a single turn, decresing 5%% per trigger.
         The damage scales with your level and cunning.
-        ]]):tformat(t.getDamage(self, t))
+        Also learning this talent will grant you immunity to others' Tricks of the Trade effect.
+        ]]):tformat(damDesc(self, DamageType.PHYSICAL, t.getDamage(self, t)))
     end
 end
 return _M
--- superload/data/talents/uber/str.lua
+++ superload/data/talents/uber/str.lua
@@ -10,9 +10,60 @@ local rush = Talents.talents_def['T_STEAMROLLER']
 if rush then
     rush.info = function(self, t)
         return ([[When you rush, the creature you rush to is marked. If you kill it in the next 2 turns then your rush cooldown is reset.
-		Each time you rush, you gain a stacking +20%% damage buff for 2 turns, up to 100%%.
-		Rush now takes 25%% time to use, and only costs 2 stamina.]]):tformat()
+        Each time you rush, you gain a stacking +20%% damage buff for 2 turns, up to 100%%.
+        Rush now takes 25%% time to use, and only costs 2 stamina.]]):tformat()
     end
 end
+local titan = Talents.talents_def['T_TITAN_S_SMASH']
+if titan then
+    titan.require = { special={desc=_t"Have dug at least 30 walls/trees/etc. and have dealt over 50000 damage with two-handed weapons", fct=function(self) return
+          		self.dug_times and self.dug_times >= 30 and
+          		self.damage_log and self.damage_log.weapon.twohanded and self.damage_log.weapon.twohanded >= 50000
+          	end},
+          	 special2={desc=_t"Be of size category 'big' or larger. This is also required to use it.", fct=function(self) return self.size_category and self.size_category >= 4 end}
+          	 }
+    titan.action = function(self, t)
+		local tg = self:getTalentTarget(t)
+		local x, y, target = self:getTarget(tg)
+		if not target or not self:canProject(tg, x, y) then return nil end
 
+		local dx, dy = (target.x - self.x), (target.y - self.y)
+		local dir = util.coordToDir(dx, dy, 0)
+		local sides = util.dirSides(dir, 0)
+
+        local destroyed = false
+
+		target:knockback(self.x, self.y, 5, function(t2, x, y)
+			if sides then
+				local d = rng.chance(2) and sides.hard_left or sides.hard_right
+				local sx, sy = util.coordAddDir(t2.x, t2.y, d)
+				local destroyed2 = false
+				t2:knockback(sx, sy, 2, function(t3) return true end)
+			end
+			if t2:canBe("stun") then t2:setEffect(t2.EFF_STUNNED, 3, {}) end
+			t2:setEffect(target.EFF_COUNTERSTRIKE, 2, {power=20, no_ct_effect=true, src=self, nb=1})
+		end,  function(g, x, y)
+            if g:attr("dig") and not destroyed then
+             	DamageType:get(DamageType.DIG).projector(self, x, y, DamageType.DIG, 1)
+             	destroyed = true
+            end
+        end)
+		if target:canBe("stun") then target:setEffect(target.EFF_STUNNED, 3, {}) end
+		if self:attackTarget(target, nil, 3.5 + 0.8 * (self.size_category - 4) + (destroyed and 1.5 or 0), true) then
+            target:setEffect(target.EFF_COUNTERSTRIKE, 2, {power=20, no_ct_effect=true, src=self, nb=1})
+        end
+        if destroyed then
+            target:setEffect(target.EFF_DAZED, 5, {})
+        end
+        target:crossTierEffect(target.EFF_OFFBALANCE, self:combatPhysicalpower())
+		return true
+	end
+    titan.info = function(self, t)
+        return ([[You deal a massive blow to your foe, smashing it for 350%% weapon damage, knocking it back 5 tiles, and knocking aside all foes in its path.
+        All targets affected are stunned for 3 turns and applied the Counterstrike effect.
+        If the knockback makes it hit a wall, it will smash down the wall, deal additional 150%% weapon damage and daze the target for 5 turns, this ignores daze immunities.
+        For each size category over 'big' you gain an additional +80%% weapon damage.]])
+        :tformat()
+    end
+end
 return _M
